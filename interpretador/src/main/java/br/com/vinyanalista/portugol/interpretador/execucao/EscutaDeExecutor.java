2465b575893f6df1250006463dc8080bdec1ae2c
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2018-04-22 23:28:24 -0300

    Criação de submódulos do Git

--- interpretador/src/main/java/br/com/vinyanalista/portugol/interpretador/execucao/EscutaDeExecutor.java
+++
@@ -1,5 +0,0 @@
-package br.com.vinyanalista.portugol.interpretador.execucao;
-
-public interface EscutaDeExecutor {
-	void aoEncerrarExecucao(ErroEmTempoDeExecucao erroEmTempoDeExecucao);
-}
--- interpretador/src/main/java/br/com/vinyanalista/portugol/interpretador/tipo/TipoSubrotinaPredefinida.java
+++
@@ -1,7 +0,0 @@
-package br.com.vinyanalista.portugol.interpretador.tipo;
-
-public class TipoSubrotinaPredefinida extends TipoSubrotina {
-	public TipoSubrotinaPredefinida() {
-		super(null, null);
-	}
-}
